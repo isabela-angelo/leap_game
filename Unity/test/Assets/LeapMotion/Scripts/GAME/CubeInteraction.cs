6e5d9ac1d87ac6663679ff9289f617c237e245d4
isabela-angelo <isabela.angelo@gmail.com>
2016-11-22 20:02:56 -0200

    first commit

---
+++ Unity/test/Assets/LeapMotion/Scripts/GAME/CubeInteraction.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+using System.Collections.Generic;
+using System;
+
+public class CubeInteraction : MonoBehaviour
+{
+	public AudioClip dindon;
+	private AudioSource source;
+	private bool flag_right = true;
+	private bool flag_left = true;
+	Controller controller;
+
+	string whichHand (GameObject obj) {
+		name = obj.transform.parent.gameObject.transform.parent.gameObject.name;
+		//Debug.Log (name);
+		if (name.Contains ("RigidRoundHand_R")) {
+			return "right";
+		}
+		else if (name.Contains ("RigidRoundHand_L")){
+			return "left";
+		}
+		return "error";
+	}
+					
+		
+
+
+	void OnTriggerEnter (Collider col)
+	{			
+		//Debug.Log ("+"+col.gameObject.name+"+");
+		if (col.gameObject.name.Equals("bone3")) {
+			source = GetComponent<AudioSource> ();
+			if (flag_right && whichHand (col.gameObject).Equals ("right")) {
+				Debug.Log ("mão direita!");
+				source.panStereo = 1.0f;
+				flag_right = false;
+				source.PlayOneShot (dindon, 1);
+
+			}
+			else if (flag_left && whichHand (col.gameObject).Equals ("left")) {
+				Debug.Log ("mão esquerda!");
+				source.panStereo = -1.0f;
+				flag_left = false;
+				source.PlayOneShot (dindon, 1);
+			}
+		}
+	}
+	void OnTriggerExit (Collider col) {
+		if (col.gameObject.name.Equals("bone3")) {
+			if (whichHand (col.gameObject).Equals ("right")) {				
+				flag_right = true;
+			} else if (whichHand (col.gameObject).Equals ("left")) {				
+				flag_left = true;
+			}
+		}
+	}
+}
---
+++ Unity/test/Assets/LeapMotion/Scripts/GAME/HandsInteraction.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class HandsInteraction : MonoBehaviour {
+
+	Controller controller;
+	bool flag_right = true;
+	bool flag_left = true;
+
+	bool grabbing (Hand hand) {
+		//Debug.Log ("+"+hand.GrabStrength+"+");
+		if (hand.GrabStrength > 0.5){
+			return true;
+		}
+		return false;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {	
+		controller = new Controller ();
+		Frame frame = controller.Frame ();
+		bool left = false;
+		bool right = false;
+		foreach (Hand hand in frame.Hands) {
+			if (grabbing(hand)) {
+				if (flag_left && hand.IsLeft) {
+					//Debug.Log ("left");
+					left = true;
+					flag_left = false;
+				} else {
+					flag_left = true;
+				}
+				if (flag_right && hand.IsRight) {
+					//Debug.Log ("right");
+					right = true;
+					flag_right = false;
+				} else {
+					flag_right = true;
+				}
+			}
+			if (right && left) {
+				Debug.Log ("mapa!");
+			} else if (right) {
+				Debug.Log ("vira direita!");				
+			} else if (left) {
+				Debug.Log ("vira esquerda!");				
+			}				
+		}
+	}
+}
